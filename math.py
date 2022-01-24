0e7b74a40e5dd1e3bfac14468e8eb28eccc0075c
nitishkap <nitish5571@gmail.com>
2022-01-25 01:02:19 +0530

    added fn header

--- math.py
+++ math.py
@@ -1,11 +1,16 @@
+#add implementation
 def add(x,y):
     pass    
 
+#sub implementaion
 def subtract(x,y):
     pass
 
+#multiply implementaion
 def multiply(x,y):
     pass
 
+
+#div implementaion
 def divide(x,y):
     pass
